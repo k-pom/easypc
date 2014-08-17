d11d312e2ca68adcd0e8fdcf2ce1ea151bbbecd9
Kaleb Pomeroy <kaleb.pomeroy@rackspace.com>
2014-08-17 00:55:29 -0500

    Databases working with heroku

--- easypc/database.py
+++ easypc/database.py
@@ -1,5 +1,17 @@
+import os
 from pymongo import MongoClient
 from easypc.config import config
 
-client = MongoClient(config['mongodb']['host'], config['mongodb']['port'])
-db = client.easypc
+client = MongoClient(
+    os.environ.get('MONGODB_HOST', config['mongodb']['host']),
+    os.environ.get('MONGODB_PORT', config['mongodb']['port'])
+)
+
+database = os.environ.get('MONGODB_DATABASE', config['mongodb']['database'])
+db = client[database]
+
+user = os.environ.get('MONGODB_USER', config['mongodb']['user'])
+pw = os.environ.get('MONGODB_PASS', config['mongodb']['pass'])
+
+if user and pw:
+    db.authenticate(user, pw)
